cf8c21e659f94775f793bcc65d6e800ee9e1d7ad
hdmuae <yazan@hdmuae.com>
2022-03-15 11:45:01 +0400

    fix: about section background

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -3,20 +3,19 @@ import Image from "next/image";
 
 export const About: React.FC = () => {
   return (
-    <section className="relative h-[700px] bg-darkblue bg-[url('/images/coffee.png')] bg-cover">
-      {/* <Image
-        className="-z-10"
+    <section className="relative h-[850px] bg-darkblue">
+      <Image
+        className="z-0"
         layout="fill"
-        objectFit="cover"
         alt="coffee background"
-        src="/images/Subtract.png"
-      /> */}
+        src="/images/coffee.png"
+      />
 
       <div className="flex h-full w-4/5 max-w-default flex-col justify-center lg:mx-12 ground:mx-auto">
-        <h1 className="pt-12 pb-12 font-minion text-4xl font-bold text-white lg:text-title">
+        <h1 className="z-10 pt-12 pb-12 font-minion text-4xl font-bold text-white lg:text-title">
           About us
         </h1>
-        <p className="w-full font-nexa text-lg text-white lg:w-8/12">
+        <p className="z-10 w-full font-nexa text-lg text-white lg:w-8/12">
           The west wind has brought real inspiration from those lands where the
           long-lasting coffee culture soars, saturated with history, heritage
           and inspiration. From where it all began and what we want to convey to
