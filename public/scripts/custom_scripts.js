b8a5f6e70a1537fdb25926dc2a2ad63c4dee9c4a
HDM Marketing <95614770+hdmuae@users.noreply.github.com>
2022-06-04 02:58:33 +0300

    Update custom_scripts.js

--- public/scripts/custom_scripts.js
+++ public/scripts/custom_scripts.js
@@ -1,1 +1,5 @@
-alert('Hello');
+var readMoreBtn = document.getElementsByClassName('read_more');
+readMoreBtn.onclick = function() {
+  var hiddenText = document.getElementsByClassName('hidden');
+  hiddenText.classList.remove("hidden");
+};
