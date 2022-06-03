2d811b071398c579f0059de2423789df4f672c57
HDM Marketing <95614770+hdmuae@users.noreply.github.com>
2022-06-04 02:55:38 +0300

    Update my_styles.js

--- src/containers/About/my_styles.js
+++ src/containers/About/my_styles.js
@@ -1,4 +1,5 @@
-function readMore() {
-  var readMoreBtn = document.getElementsByClassName('read_more');
-  readMoreBtn.classList.remove("hidden");
-}
+var readMoreBtn = document.getElementsByClassName('read_more');
+readMoreBtn.onclick = function() {
+  var hiddenText = document.getElementsByClassName('hidden');
+  hiddenText.classList.remove("hidden");
+};
