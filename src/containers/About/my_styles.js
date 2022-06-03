6d495a6c5c73ca458b66ce58c40133b04b98a06c
HDM Marketing <95614770+hdmuae@users.noreply.github.com>
2022-06-04 02:59:18 +0300

    Delete my_styles.js

--- src/containers/About/my_styles.js
+++
@@ -1,5 +0,0 @@
-var readMoreBtn = document.getElementsByClassName('read_more');
-readMoreBtn.onclick = function() {
-  var hiddenText = document.getElementsByClassName('hidden');
-  hiddenText.classList.remove("hidden");
-};
