01233dd8c8641eb08e855220e6b0e9acbd77fcbc
HDM Marketing <95614770+hdmuae@users.noreply.github.com>
2022-06-04 02:39:48 +0300

    Update index.tsx

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -26,17 +26,17 @@ export const About: React.FC = () => {
           The west wind has brought real inspiration from those lands where the long-lasting coffee culture soars, saturated with history, 
           heritage and inspiration. From where it all began and what we want to convey to the city dwellers.
         </p>
-        <p className="z-10 w-full font-nexa text-lg text-white lg:w-8/12 mb-6">
+        <p className="z-10 w-full font-nexa text-lg text-white lg:w-8/12 mb-6 hidden">
           We have created a clear space for your imagination and inspiration in pure minimalistic design, with secret details that distinguish 
           our brand. Our coffee made the astonishing trip to satisfy you and make your life better because you are worthy to drink the delicious coffee.
         </p>
-        <p className="z-10 w-full font-nexa text-lg text-white lg:w-8/12 mb-6">
+        <p className="z-10 w-full font-nexa text-lg text-white lg:w-8/12 mb-6 hidden">
           In your cup -  first-class quality coffee beans — Dersut, originally from the picturesque city of Conegliano, Treviso, Italy. Many varieties 
           are even Espresso Italiano certified and have won at least one Gold Medal at the International Coffee Tasting, Dersut also became a member of 
           SCA (Speciality Coffee Association), a world association that promotes quality coffee culture and specialty coffees. The pleasure of excellent 
           Italian coffee is guaranteed by Dersut quality.
         </p>
-        <p className="z-10 w-full font-nexa text-lg text-white lg:w-8/12 mb-6">
+        <p className="z-10 w-full font-nexa text-lg text-white lg:w-8/12 mb-6 hidden">
           We are the first coffee shop in Dubai where you can try Dersut espresso and other traditional Italian coffee drinks in company with your 
           favorite desserts and handmade chocolate. Moreover we are official distributor of Dersut in UAE, the selection of products you can find 
           in our <a href="#">Shop</a>.
