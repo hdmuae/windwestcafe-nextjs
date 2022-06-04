a23c05e0a218c4eec516fe102d2b354a747a8e13
Fedor <matskofedor@gmail.com>
2022-06-05 02:16:45 +0300

    menu changes

--- src/components/MenuTable/index.tsx
+++ src/components/MenuTable/index.tsx
@@ -36,6 +36,25 @@ const Menu: React.FC = () => {
       </div>
     </div>
   );
+  const SubMenuRecord: React.FC<{
+    name: string;
+    description: string;
+    price: number;
+  }> = ({ name, description, price }) => (
+      <div className="mb-4 flex items-center justify-between border-b-2 border-gray-300 pb-2">
+        <div className="flex w-3/4 flex-col justify-between">
+          <p className="mb-2 font-minion text-lg font-bold pl-8">{name}</p>
+          <p className="w-3/4 font-nexa text-sm font-light text-primary opacity-50 lg:w-full pl-8">
+            {description}
+          </p>
+        </div>
+        <div className="w-1/4">
+          <p className="font-minion text-2xl font-bold md:text-right">
+            {price} AED
+          </p>
+        </div>
+      </div>
+  );
 
   return (
     <div className="flex flex-col lg:flex-row">
@@ -114,29 +133,79 @@ const Menu: React.FC = () => {
 
       <div className="m-auto w-11/12">
         <MenuRecord
-          name="Cappuchino"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          price={25}
-        />
-        <MenuRecord
-          name="Hot Chocolate"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          price={35}
-        />
-        <MenuRecord
-          name="Latte"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor"
-          price={19}
-        />
-        <MenuRecord
           name="Espresso"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-          price={15}
+          price={18}
+        />
+        <SubMenuRecord
+            name="Double"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            price={22}
+        />
+        <SubMenuRecord
+            name="Freddo"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            price={23}
+        />
+        <SubMenuRecord
+            name="Tonic"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            price={27}
         />
         <MenuRecord
-          name="Fappuchino"
+          name="Americano"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          price={18}
+        />
+        <MenuRecord
+          name="Cappuccino"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor"
+          price={21}
+        />
+        <SubMenuRecord
+            name="Freddo"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            price={24}
+        />
+        <MenuRecord
+          name="Late"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+          price={22}
+        />
+        <SubMenuRecord
+            name="Iced"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            price={22}
+        />
+        <SubMenuRecord
+            name="Spanish"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            price={26}
+        />
+        <SubMenuRecord
+            name="Vanilla"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            price={24}
+        />
+        <MenuRecord
+          name="Flat White"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor"
           price={25}
+        />
+        <SubMenuRecord
+            name="Freddo"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+            price={27}
+        />
+        <MenuRecord
+            name="Flat White"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor"
+            price={25}
+        />
+        <MenuRecord
+            name="Cacao"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor"
+            price={22}
         />
       </div>
     </div>
--- src/containers/Menu/index.tsx
+++ src/containers/Menu/index.tsx
@@ -8,9 +8,13 @@ export const Menu: React.FC = () => {
         <h1 className="mb-12 font-minion text-5xl font-bold lg:text-title">
           Menu
         </h1>
-          <p className="z-10 w-full font-nexa text-lg lg:w-8/12 mb-12">The west wind has brought real
-              inspiration from those lands where the long-lasting coffee culture soars, saturated with history, heritage
-              and inspiration. From where it all began and what we want to convey to the city dwellers.</p>
+          <p className="z-10 w-full font-nexa text-lg lg:w-8/12 mb-12">Wind West Cafe delivers way beyond the traditional
+              coffee shop favorites. Full selection of classic coffee drinks and as well absolutely unique Dersut special
+              coffee cocktails. Filter coffee appreciators we are ready to surprise you with our impeccable fine taste and
+              innovations in the coffee world. You will also find your favorite teas, fresh juices and healthy smoothies.
+              Our delicious dessert and handmade chocolate can make a blast of your taste buds together with your favorite
+              drinks.
+          </p>
 
         <MenuTable />
       </div>
