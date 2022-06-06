877e7d3afd8c2e4273482abc020f51c54446eee2
Fedor <matskofedor@gmail.com>
2022-06-06 04:20:25 +0300

    map thumb contact2

--- src/containers/ContactUs/index.tsx
+++ src/containers/ContactUs/index.tsx
@@ -97,7 +97,7 @@ export const ContactUs: React.FC = () => {
         </form>
           <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3613.044705348557!2d55.17650021448789!3d25.100347983941347!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6ba659a5ad91%3A0x279d1db038a4a880!2sWind%20West%20Home!5e0!3m2!1sru!2sua!4v1654478061047!5m2!1sru!2sua"
-              width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy"
+              width="600" height="450" style={{border:0}} loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"></iframe>
         </div>
         <ToastContainer />
