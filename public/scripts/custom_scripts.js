8133f7249c4e3c24fa7b1e10698b2fd70b89a0b7
Fedor <matskofedor@gmail.com>
2022-06-07 05:26:19 +0300

    custom visible\hidden

--- public/scripts/custom_scripts.js
+++ public/scripts/custom_scripts.js
@@ -1,19 +1,19 @@
 var clickBtn = document.querySelector('.read_more');
 clickBtn.addEventListener("click", function() {
   if(clickBtn.classList.contains('hidden_text')){
-    var elements = document.querySelectorAll(".hidden");
+    var elements = document.querySelectorAll(".chidden");
     elements.forEach(function(element) {
-      element.classList.remove("hidden");
-      element.classList.add("visible");
+      element.classList.remove("chidden");
+      element.classList.add("cvisible");
     });
     clickBtn.innerHTML = 'Show Less';
     clickBtn.classList.remove("hidden_text");
     clickBtn.classList.add("visible_text");
   }else {
-    var elements = document.querySelectorAll(".visible");
+    var elements = document.querySelectorAll(".cvisible");
     elements.forEach(function(element) {
-      element.classList.remove("visible");
-      element.classList.add("hidden");
+      element.classList.remove("cvisible");
+      element.classList.add("chidden");
     });
     clickBtn.innerHTML = 'Read More';
     clickBtn.classList.remove("visible_text");
